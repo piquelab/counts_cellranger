5b285b626b1a98fed56d2be2bfe175b2883d8d67
rpique <rpique@gmail.com>
2021-07-30 12:33:32 -0400

    Fixed script to make ref.vcf for Covid-19 project

--- genotypes/1_make_basic_ref_vcf.sh
+++ genotypes/1_make_basic_ref_vcf.sh
@@ -4,15 +4,16 @@ set -e
 
 
 ##ln -s /nfs/rprdata/scilab/labor2/PRB_Geno_UofM/RP/outvcf/split/imputed/trimerge.vcf.gz
-ln -s /wsu/home/groups/prbgenomics/genotyping-data/PRB_Geno_UofM/Prjt_359_Pique-Regi_20201217_2535-RP/imputed.UM/2021-01-12_merge.vcf.gz
-
+##ln -s /wsu/home/groups/prbgenomics/genotyping-data/PRB_Geno_UofM/Prjt_359_Pique-Regi_20201217_2535-RP/imputed.UM/2021-01-12_merge.vcf.gz
+ln -s /wsu/home/groups/prbgenomics/genotyping-data/PRB_Geno_UofM/mergeAll.2021-07-29/2021-07-29_merge.vcf.gz 
+ln -s /wsu/home/groups/prbgenomics/genotyping-data/PRB_Geno_UofM/mergeAll.2021-07-29/2021-07-29_merge.vcf.gz.csi
 
 ## We may need to fix this, to only use the samples we need
 ## removing covid-19 samples from the vcf file. 
 
 ## Selects bi-allelic SNPs with MAF>0 and from the samples that are present, and renames to HPL-F/M 
 ## This creates the master reference genotype file. 
-bcftools view -S <(tail -n +2 Covid19GenotypeFixes.txt | cut -f2) -v snps -m2 -M2 -i 'INFO/MAF>0' trimerge.vcf.gz --threads 4 \
+bcftools view -S <(tail -n +2 Covid19GenotypeFixes.txt | cut -f2) -v snps -m2 -M2 -i 'INFO/MAF>0' mergeAll.2021-07-29/2021-07-29_merge.vcf.gz --threads 4 \
   | bcftools reheader -s <(tail -n +2 Covid19GenotypeFixes.txt | cut -f5) --threads 4 \
   | bcftools view -Oz -o ref.vcf.gz --threads 4
 
