dcb3cd2f61b1ba7be76a685aa709e239dfc343f6
rpique <rpique@gmail.com>
2024-04-22 10:12:03 -0400

    Monitor mem usage and cpu time of fastdemux

--- fastdemux/demuxOne.sh
+++ fastdemux/demuxOne.sh
@@ -21,13 +21,15 @@ do
 set -v 
 set -e
 module load misc; 
+echo "SLURM_JOB_ID:"\$SLURM_JOB_ID
+echo "SLURM_JOB_NAME:"\$SLURM_JOB_NAME
 time fastdemux -t ${ncpus} ../${sample}/possorted_genome_bam.bam ${vcfFile} ../${sample}/raw_feature_bc_matrix/barcodes.tsv.gz ${demuxFolder}/${sample}.fdout.raw 
+sacct -j \$SLURM_JOB_ID --format=JobID,MaxRSS,CPUTime,AveRSS --parsable2
 EOF
     fi
 done
 
 ##
-
 
 ##| qsub -q wsuq -l mem=120gb -l ncpus=2 -N $sample${2}
 ###combined.posG9.reordered.vcf.gz
