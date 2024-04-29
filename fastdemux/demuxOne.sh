c32642b7e978cfdf6b2df4cc16e031bff39ce843
rpique <rpique@gmail.com>
2024-04-29 12:21:10 -0400

    Fixing some mistakes that I did by introducing some ATAC names in the RNA pipeline.

--- fastdemux/demuxOne.sh
+++ fastdemux/demuxOne.sh
@@ -23,12 +23,11 @@ set -e
 module load misc; 
 echo "SLURM_JOB_ID:"\$SLURM_JOB_ID
 echo "SLURM_JOB_NAME:"\$SLURM_JOB_NAME
-time fastdemux -t ${ncpus} ../${sample}/possorted_bam.bam ${vcfFile} ../${sample}/raw_peak_bc_matrix/barcodes.tsv ${demuxFolder}/${sample}.fdout.raw 
+time fastdemux -t ${ncpus} ../${sample}/possorted_genome_bam.bam ${vcfFile} ../${sample}/raw_feature_bc_matrix/barcodes.tsv.gz ${demuxFolder}/${sample}.fdout.raw 
 sacct -j \$SLURM_JOB_ID --format=JobID,MaxRSS,CPUTime,AveRSS --parsable2
 EOF
     fi
 done
 
-##time ./fastdemux -t 16 /rs/rs_grp_schold/CZI/ATAC/counts_cellranger_atac/HOLD2-ATAC-CTRL/possorted_bam.bam  /rs/rs_grp_schold/CZI/ATAC/counts_cellranger_atac/genotypes/combined.posG9.reheader.vcf.gz /rs/rs_grp_schold/CZI/ATAC/counts_cellranger_atac/HOLD2-ATAC-CTRL/raw_peak_bc_matrix/barcodes.tsv out.HOLD2-ATAC-CTRL.full 
 ##| qsub -q wsuq -l mem=120gb -l ncpus=2 -N $sample${2}
 ###combined.posG9.reordered.vcf.gz
